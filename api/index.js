091b3bc6815c2f8c417d14fbfa721f613a04e9df
kattamuriNagabhushan <nbhushan52@gmail.com>
2024-08-06 03:38:03 +0530

    db connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(
+    () =>{
+        console.log("db connection successful");
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+
 
 const app = express()
 
