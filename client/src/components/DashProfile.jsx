59ecbf5da014a6de06f4a260968f83158cf4b6fa
kattamuriNagabhushan <nbhushan52@gmail.com>
2024-08-23 23:37:58 +0530

    created user upload image

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,8 +8,8 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-// import { CircularProgressbar } from 'react-circular-progressbar';
-// import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import {
   updateStart,
   updateSuccess,
